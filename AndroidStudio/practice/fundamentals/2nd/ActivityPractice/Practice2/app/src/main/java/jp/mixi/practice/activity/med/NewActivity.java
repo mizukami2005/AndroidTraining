59e74e44ecb02854468d35a9185fd1c96fe41384
Takamasa Mizukami <mizukami2005@gmail.com>
2016-08-07 16:17:08 +0900

    Activity
    2終了

---
+++ AndroidStudio/practice/fundamentals/2nd/ActivityPractice/Practice2/app/src/main/java/jp/mixi/practice/activity/med/NewActivity.java
@@ -0,0 +1,13 @@
+package jp.mixi.practice.activity.med;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
